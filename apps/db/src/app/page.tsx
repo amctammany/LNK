1a37be457bc4a7325f9340637f4fcb61fe2838ea
Alex McTammany <apmctammany@gmail.com>
2023-07-11 11:13:51 -0400

    foo

--- apps/db/src/app/page.tsx
+++ apps/db/src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-import p from "@prisma/client";
+import p from "@/graphql/schema";
 export default async function Home() {
   return (
     <main className={styles.main}>
