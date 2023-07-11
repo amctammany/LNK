d60d116ef6f5ab131ec6771b47fd1404d89eb24e
Alex McTammany <apmctammany@gmail.com>
2023-07-11 09:13:46 -0400

    added apollo client to web.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,22 @@
+import { gql } from "@apollo/client";
 import { Button, Header } from "ui";
+import { getClient } from "../lib/client";
 
-export default function Page() {
+const query = gql`
+  {
+    findManyRecipe {
+      name
+    }
+  }
+`;
+export default async function Page() {
+  const client = getClient();
+  const { data } = await client.query({ query });
   return (
     <>
       <Header text="Web" />
       <Button />
+      <p>{JSON.stringify(data)}</p>
     </>
   );
 }
