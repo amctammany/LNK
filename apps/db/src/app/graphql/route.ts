e03251fce5e73d73f9401735cc10e909009893df
Alex McTammany <apmctammany@gmail.com>
2023-07-11 10:46:09 -0400

    prisma???

--- apps/db/src/app/graphql/route.ts
+++ apps/db/src/app/graphql/route.ts
@@ -2,12 +2,15 @@ import schema from "../../graphql/schema";
 
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import prisma from "@/lib/prisma";
 
 const server = new ApolloServer({
   schema,
 });
 
-const handler = startServerAndCreateNextHandler(server);
+const handler = startServerAndCreateNextHandler(server, {
+  context: async () => ({ prisma: prisma }),
+});
 
 export async function GET(request: Request) {
   return handler(request);
