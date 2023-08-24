e685a924f1ead3534217a5287eb7a3313b2695b6
Alex McTammany <apmctammany@gmail.com>
2023-08-24 05:38:37 -0400

    added error link to web apollo client

--- apps/web/lib/client.ts
+++ apps/web/lib/client.ts
@@ -1,11 +1,22 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, from, HttpLink, InMemoryCache } from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
+import { onError } from "@apollo/client/link/error";
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+const httpLink = new HttpLink({
+  uri: process.env.APOLLO_SERVER_URI,
+});
 export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: new HttpLink({
-      uri: process.env.APOLLO_SERVER_URI,
-    }),
+    link: from([errorLink, httpLink]),
   });
 });
